4dd2649d6f83a5fc3d0600a8410e58a8a8d6cedd
Arman229 <armanashraf015@gmail.com>
2024-02-12 17:48:56 +0500

    Complete watching and practicing the code from Class 2 on GitHub.

--- Code_practics/python/chapter3/README.md
+++ Code_practics/python/chapter3/README.md
@@ -1,1 +1,4 @@
 # Chapter 3 List
+# List Methods
+# Solution of exercise
+# How we can seek assistance in python?
--- Code_practics/python/chapter3/index.py
+++ Code_practics/python/chapter3/index.py
@@ -1,3 +1,19 @@
-print('hello world')
+
+# from typing import Any
+# print('hello world')
  
-list_of_name:list[int]=[2,3]
+# list_of_name:list[int]=[2,3]
+
+                 
+# names : list[str] = ["Qasim","Sir Zia", "Sir Inam"]
+                    
+
+# print(names[-3])
+# alphabatics:list[Any] = list('abcdefghijklmnopqrstuvwxyz')
+# alphabatics2:list[str] = list('abcdefghijklmnopqrstuvwxyz')
+# print(alphabatics)
+# print(alphabatics2)
+ 
+help(list)
+
+ 
